0fec00991d9ff49162ab93c0e7c31ecca4c807fa
Sundarm <sundarm.soni097@gmail.com>
2024-01-13 18:08:05 +0530

    feature/ngrx ecommerce project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { NxWelcomeComponent } from './nx-welcome.component'
+import { HomeComponent } from '@angular-ecommerce/home';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HomeComponent],
   selector: 'angular-ecommerce-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideAnimations(),
+    provideState('', {}),
+    provideEffects([])
+  ],
 };
